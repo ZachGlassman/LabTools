22daae20e6e2b57e7b989ba54b079dc6ea3c159f
zachglassman <zach.glassman@gmail.com>
2017-01-26 20:07:48 -0500

    finds values

--- labscript_desc.py
+++ labscript_desc.py
@@ -59,6 +59,81 @@ def get_run_info(run, folder_path):
                 number = str(int(number) + 1)
     return ans
 
+def format_numerical_param(x):
+    """return numerical parameter as string"""
+    if abs(x) > 1:
+        y = x
+        extra = ''
+    elif x < 1 and x > 1e-3:
+        y = 1e3 * x
+        extra = 'm'
+    elif x < 1e-3 and x > 1e-6:
+        y = 1e6 * x
+        extra = 'mu'
+    else:
+        print(x)
+    return "{:.3f}{}".format(y, extra)
+
+
+def full_output(run_info):
+    _str = ''
+    for k, v in run_info.items():
+        if len(v) > 1:
+            _str += 'Run {}:\n'.format(k)
+            for kk, vv in v.items():
+                param_str = ','.join(format_numerical_param(i) for i in vv)
+                _str += '  Param {}\n'.format(kk)
+                _str += '    Values: {}\n'.format(param_str)
+                _str += '    Min: {}\n'.format(format_numerical_param(min(vv)))
+                _str += '    Max: {}\n'.format(format_numerical_param(max(vv)))
+                _str += '    Len: {}\n'.format(len(vv))
+            _str += ''.join('#' for i in range(50)) + '\n'
+
+    return _str
+
+def summary_ouput(run_info):
+    form_str = '{0:40}|{1:40}\n'
+    _str = form_str.format('Run','Scan Params')
+    _str += ''.join('-' for i in range(80)) + '\n'
+    for k, v in run_info.items():
+        if len(v) > 1:
+            _str += form_str.format(k, ', '.join(i for i in v))
+
+    return _str
+
+def dict_equal(d1, d2):
+    #first check if keys are equal
+    if d1.keys() != d2.keys():
+        return False, False
+    else:
+        same_vals = True
+        for k,v in d1.items():
+            try:
+                if v != d2[k]:
+                    same_vals = False
+            except ValueError:
+                if v.all() != d2[k].all():
+                    same_vals = False
+        return True, same_vals
+
+def find_same_scans(run_info):
+    #get list of tuples with same scan params
+    # dict isn't hashable so can't use set, will use stupid algorithm, but will be find for low numbers
+    a = {k:v for k,v in run_info.items() if len(v) > 1}
+    keys = list(a.keys())
+    ans = []
+    for i in range(len(a)-1):
+        key = keys.pop(0)
+        for k,v in a.items():
+            same_keys, same_values = dict_equal(a[key], v)
+            if same_keys and k != key:
+                ans.append([key,k, same_values])
+
+    _str = "Scans with same scanned parameters are :\n"
+    for i in ans:
+        _str += "{}, {} :\n Same vals : {}\n".format(i[0],i[1],i[2])
+
+    return _str
 def main(folder_path):
     all_files = os.listdir(folder_path)
     runs = set([get_run_name(i) for i in all_files])
@@ -66,13 +141,13 @@ def main(folder_path):
     run_info = OrderedDict()
     for run in runs:
         run_info[run] = get_run_info(run, folder_path)
-    print("Total of {} shots, with {} runs".format(len(all_files), len(runs)))
-    for k,v in run_info.items():
-        if len(v) > 1:
-            print('Run {}:'.format(k))
-            for kk,vv in v.items():
-                param_str = ','.join(str(i) for i in vv)
-                print('\t Param {} : {}'.format(kk, param_str))
+    #now create output
+    _str = "Total of {} shots, with {} runs\n".format(len(all_files), len(runs))
+    #_str += full_output(run_info)
+    _str += summary_ouput(run_info)
+    _str += find_same_scans(run_info)
+    print(_str)
+    
 
 
 if __name__ == '__main__':
